6dec3d6a4b003727b701a28813e38204d39f4cc6
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-13 16:55:12 +0530

    Remove unused CSS variables and update layout component with a heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <h1>This is layout page</h1>
         {children}
       </body>
     </html>
