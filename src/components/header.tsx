cc97445a593705452a2c6b4172262cc4815a594f
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-15 15:56:55 +0530

    Refactor Navbar component: replace headings with links for navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const NavbarPage = () => {
   return (
     <div>
       <nav className="container flex items-center justify-between mx-auto h-14">
-        <h1 className="font-bold text-xl">Home</h1>
+        <Link href="/" className="font-bold text-xl">Home</Link>
         <div className="flex gap-2">
-          <h1>Performance</h1>
-          <h1>Reliabilty</h1>
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliabilty">Reliabilty</Link>
         </div>
       </nav>
     </div>
